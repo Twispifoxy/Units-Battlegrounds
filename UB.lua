ed6e5b252e4b697f86984f3b49a73833f9ba58c7
Twispifoxy <twispifoxy@mail.ru>
2025-03-07 20:31:05 +0300

    Update UB.lua

--- UB.lua
+++ UB.lua
@@ -1,1 +1,9 @@
-﻿print("we have nothing:(")
+﻿repeat wait();
+until game:IsLoaded()
+
+print("I'm running :)");
+
+local source = "https://raw.githubusercontent.com/Twispifoxy/Units-Battlegrounds/refs/heads/main/UB.lua";
+
+local teleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport); 
+teleport("loadstring(game:HttpGet('"   .. source   .. "'))()" );
