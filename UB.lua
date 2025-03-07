7e5af0a9a88050e64cd99aad19f619c681573c34
Twispifoxy <twispifoxy@mail.ru>
2025-03-07 21:01:48 +0300

    Update UB.lua

--- UB.lua
+++ UB.lua
@@ -3,8 +3,6 @@
 local player = game.Players.LocalPlayer
 local source = "https://raw.githubusercontent.com/Twispifoxy/Units-Battlegrounds/refs/heads/main/UB.lua"
 
-player.CharacterAdded:Connect(function()
-    wait(1)
-	print("I'm running :)")
-    loadstring(game:HttpGet(source))()
-end)
+print("on")
+
+queue_on_teleport("loadstring(game:HttpGet('"   .. source   .. "'))()" );
